b8b6d06c2f8dfbf933eaa305cbd94101b0365904
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-10-29 20:59:20 +0530

    Vijay - 2017/10/29 - Design patterns examples

---
+++ creational-patterns/src/org/pjay/designpatterns/factory/example2/Ak47.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.factory.example2;
+
+/**
+ * @author Vijay
+ *
+ */
+public class Ak47 extends Gun{
+
+	public Ak47() {
+		setGunName("AK 47");
+		setAmountOfDamage("75%");
+		setAttackDistance("350 meters");
+		setNumberOfBulletsLoaded(47);
+	}
+	
+}
