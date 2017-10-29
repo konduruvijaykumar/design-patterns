c7223e0de862f196c03c3ee6d538cc2d246fd782
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-10-29 20:59:30 +0530

    Vijay - 2017/10/29 - Design patterns examples

---
+++ creational-patterns/src/org/pjay/designpatterns/factory/example2/SawOffGun.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.factory.example2;
+
+/**
+ * @author Vijay
+ *
+ */
+public class SawOffGun extends Gun{
+
+	public SawOffGun() {
+		setGunName("SawOffGun");
+		setAmountOfDamage("60%");
+		setAttackDistance("250 meters");
+		setNumberOfBulletsLoaded(2);
+	}
+	
+}
