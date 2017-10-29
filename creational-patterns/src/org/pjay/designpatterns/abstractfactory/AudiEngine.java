6a7186f8617390c0817530949b7a9b2777ca9150
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-10-29 19:12:38 +0530

    Vijay - 2017/10/29 - Design patterns examples

---
+++ creational-patterns/src/org/pjay/designpatterns/abstractfactory/AudiEngine.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.abstractfactory;
+
+/**
+ * @author vijayk
+ *
+ */
+public class AudiEngine implements Engine {
+
+	@Override
+	public void createEngine() {
+		System.out.println("Creating Audi Engine... ");
+	}
+
+}
