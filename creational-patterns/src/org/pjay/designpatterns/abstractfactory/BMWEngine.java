e8e144757c1c91a395752e85a0600c7fcb8930a1
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-10-29 19:12:23 +0530

    Vijay - 2017/10/29 - Design patterns examples

---
+++ creational-patterns/src/org/pjay/designpatterns/abstractfactory/BMWEngine.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.abstractfactory;
+
+/**
+ * @author vijayk
+ *
+ */
+public class BMWEngine implements Engine {
+
+	@Override
+	public void createEngine() {
+		System.out.println("Creating BMW Engine... ");
+	}
+
+}
