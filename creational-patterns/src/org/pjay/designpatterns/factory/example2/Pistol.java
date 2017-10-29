60f810e004842fa800d647ab051315cd63ea31d3
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-10-29 20:59:25 +0530

    Vijay - 2017/10/29 - Design patterns examples

---
+++ creational-patterns/src/org/pjay/designpatterns/factory/example2/Pistol.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.factory.example2;
+
+/**
+ * @author Vijay
+ *
+ */
+public class Pistol extends Gun{
+
+	public Pistol() {
+		setGunName("Pistol");
+		setAmountOfDamage("40%");
+		setAttackDistance("300 meters");
+		setNumberOfBulletsLoaded(9);
+	}
+	
+}
