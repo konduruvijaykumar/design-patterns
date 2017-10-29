6a9332626ae1e8f3d8a051d9a8ac4416399e403c
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-10-29 20:59:33 +0530

    Vijay - 2017/10/29 - Design patterns examples

---
+++ creational-patterns/src/org/pjay/designpatterns/factory/example2/Sniper.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.factory.example2;
+
+/**
+ * @author Vijay
+ *
+ */
+public class Sniper extends Gun{
+
+	public Sniper() {
+		setGunName("Sniper");
+		setAmountOfDamage("90%");
+		setAttackDistance("1000 meters");
+		setNumberOfBulletsLoaded(1);
+	}
+	
+}
