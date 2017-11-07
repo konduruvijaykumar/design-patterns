0e44fdfca722fecdb1444b766a092e2b5a7d2c09
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-11-08 01:44:02 +0530

    Vijay - 2017/11/08 - Design patterns examples

---
+++ creational-patterns/src/org/pjay/designpatterns/builder/example2/BrickFloor.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.builder.example2;
+
+/**
+ * @author vijayk
+ *
+ */
+public class BrickFloor implements Floor {
+
+	@Override
+	public String getInfo() {
+		return "Finished building brick floor";
+	}
+
+}
