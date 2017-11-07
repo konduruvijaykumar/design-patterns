725a3b9963d6018361d30cd4bce230613c94bf8d
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-11-08 01:43:39 +0530

    Vijay - 2017/11/08 - Design patterns examples

---
+++ creational-patterns/src/org/pjay/designpatterns/builder/example2/WoodFloor.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.builder.example2;
+
+/**
+ * @author vijayk
+ *
+ */
+public class WoodFloor implements Floor {
+
+	@Override
+	public String getInfo() {
+		return "Finished building wood floor";
+	}
+
+}
