330fb902ffa18cd02265275e6dc439d518bacef5
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-11-08 01:43:45 +0530

    Vijay - 2017/11/08 - Design patterns examples

---
+++ creational-patterns/src/org/pjay/designpatterns/builder/example2/WoodRoof.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.builder.example2;
+
+/**
+ * @author vijayk
+ *
+ */
+public class WoodRoof implements Roof {
+
+	@Override
+	public String getInfo() {
+		return "Finished building wood roof";
+	}
+
+}
