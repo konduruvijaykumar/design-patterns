ce528363a1826f142b0ba144647352f4c636cbae
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-11-08 01:44:27 +0530

    Vijay - 2017/11/08 - Design patterns examples

---
+++ creational-patterns/src/org/pjay/designpatterns/builder/example2/HouseBuilder.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.builder.example2;
+
+/**
+ * @author vijayk
+ *
+ */
+public abstract class HouseBuilder {
+
+	protected House house;
+	protected Floor floor;
+	protected Walls walls;
+	protected Roof roof;
+
+	public abstract House createHouse();
+
+	public abstract Floor createFloor();
+
+	public abstract Walls createWalls();
+
+	public abstract Roof createRoof();
+
+}
