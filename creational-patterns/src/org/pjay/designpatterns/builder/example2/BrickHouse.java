03f77a5bbd2a331e4c2702d79855f7c7850be71b
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-11-08 01:44:17 +0530

    Vijay - 2017/11/08 - Design patterns examples

---
+++ creational-patterns/src/org/pjay/designpatterns/builder/example2/BrickHouse.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.builder.example2;
+
+/**
+ * @author vijayk
+ *
+ */
+public class BrickHouse extends House{
+
+	@Override
+	public String getInfo() {
+		return "Building a brick house";
+	}
+
+}
