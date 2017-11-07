db252dd0a4a1c9f51584d19a482d8e9e24337063
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-11-08 01:43:49 +0530

    Vijay - 2017/11/08 - Design patterns examples

---
+++ creational-patterns/src/org/pjay/designpatterns/builder/example2/WoodWalls.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.builder.example2;
+
+/**
+ * @author vijayk
+ *
+ */
+public class WoodWalls implements Walls {
+
+	@Override
+	public String getInfo() {
+		return "Finished building wood walls";
+	}
+
+}
