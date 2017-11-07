9b08dc4fcd5b37b0dba8130f93eeadc2e90666fc
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-11-08 01:43:57 +0530

    Vijay - 2017/11/08 - Design patterns examples

---
+++ creational-patterns/src/org/pjay/designpatterns/builder/example2/WoodHouse.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.builder.example2;
+
+/**
+ * @author vijayk
+ *
+ */
+public class WoodHouse extends House{
+
+	@Override
+	public String getInfo() {
+		return "Building a wood house";
+	}
+
+}
