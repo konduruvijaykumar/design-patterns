9e904acb8f1b3b01ec1374ea23ae7c8196e542f9
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-11-08 01:44:11 +0530

    Vijay - 2017/11/08 - Design patterns examples

---
+++ creational-patterns/src/org/pjay/designpatterns/builder/example2/BrickWalls.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.builder.example2;
+
+/**
+ * @author vijayk
+ *
+ */
+public class BrickWalls implements Walls {
+
+	@Override
+	public String getInfo() {
+		return "Finished building brick walls";
+	}
+
+}
