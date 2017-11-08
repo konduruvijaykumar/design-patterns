9b2307052f15fe23120f1b88172251f4d04c8b6c
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-11-08 22:48:02 +0530

    Vijay - 2017/11/08 - Design patterns examples

---
+++ behavioral-patterns/src/org/pjay/designpatterns/strategy/example1/OperationStrategy.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.pjay.designpatterns.strategy.example1;
+
+/**
+ * @author Vijay
+ *
+ */
+public interface OperationStrategy {
+	public int doOperation(int num1, int num2);
+}
